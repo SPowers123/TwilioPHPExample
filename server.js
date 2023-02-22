58de0656180276e6aa183160838cd1f95d849794
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2023-02-21 21:12:55 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const url = require('url');
+const qs = require('querystring');
+const twilio = require('twilio');
+
+const accountSid = 'AC485d1de41a8fb1aafd19b737c8e80040';
+const authToken = 'your_auth_token_here';
+const client = twilio(accountSid, authToken);
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
+  const { message, to } = qs.parse(parsedUrl.query);
+
+  if (pathname === '/send-message' && message && to) {
+    client.messages
+      .create({
+        body: message,
+        from: '+12013904158',
+        to,
+      })
+      .then(() => {
+        res.statusCode = 200;
+        res.end('Message sent!');
+      })
+      .catch((err) => {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Error sending message');
+      });
+  } else {
+    res.statusCode = 400;
+    res.end('Invalid request');
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
