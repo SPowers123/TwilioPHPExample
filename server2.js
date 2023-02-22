cad8ba2a6bba818e106a1167eb069a6ad0d01883
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2023-02-21 21:59:41 -0500

    Update server2.js

--- server2.js
+++ server2.js
@@ -5,28 +5,28 @@ const twilio = require('twilio');
 const app = express();
 const client = twilio('AC485d1de41a8fb1aafd19b737c8e80040', '7903784f8011064ca649737b394b7c8c');
 
-// Parse incoming JSON requests
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Endpoint for sending a text message
-app.post('/send-text', async (req, res) => {
-  const { body, to } = req.body;
+app.post('/send-message', async (req, res) => {
+  const { message, to } = req.body;
 
   try {
-    const message = await client.messages.create({
+    const twilioResponse = await client.messages.create({
       to,
-      from: '<+12013904158>',
-      body,
+      from: '+2013904158',
+      body: message,
     });
-
-    res.json({ success: true, message });
+    res.json({ success: true, response: twilioResponse });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Error sending message' });
   }
 });
 
-// Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+
+
